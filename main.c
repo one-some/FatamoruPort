3f3c8f1552f45050900a16ec5f18251db09bc7c4
one-some <onesomegit@outlook.com>
2025-12-10 14:43:06 -0600

    Hello

--- main.c
+++ main.c
@@ -10,12 +10,34 @@
 #include "parse.h"
 
 typedef struct {
+	int x;
+	int y;
+} Vec2;
+
+typedef struct {
+    Texture2D normal;
+    Texture2D pressed;
+    Texture2D hover;
+} ButtonTextureCollection;
+
+typedef struct {
+	Vec2 position;
+	char* target;
+	ButtonTextureCollection textures;
+} Button;
+
+typedef struct {
     Texture2D texture;
-    bool valid;
+	Vec2 texture_offset;
+    bool texture_valid;
+
+	char* name;
 	Vector children;
 } VisualLayer;
 
 typedef struct {
+	char* name;
+
     VisualLayer base_layer;
     VisualLayer layer_zero;
     VisualLayer layer_one;
@@ -27,7 +49,9 @@ typedef struct {
 typedef struct {
 	VisualPage fore;
 	VisualPage back;
-	int pointer_pos[2];
+
+	VisualLayer* active_layer;
+	Vec2 pointer_pos;
 } VisualScene;
 
 typedef struct {
@@ -48,11 +72,11 @@ typedef struct {
 } FataState;
 
 void load(FataState* state, char* path, char* label_target) {
-    printf("\n\nLOADINF FROM %s\n\n\n", path);
-
+	state->stopped = false;
     state->node_idx = 0;
 
     if (path) {
+		printf("\n\nLOADINF FROM %s\n\n\n", path);
         state->nodes = v_new();
 
         // WARNING: When (if) we free later, the ptr will be gone! Save it!!
@@ -94,10 +118,45 @@ void load(FataState* state, char* path, char* label_target) {
 
 }
 
+ButtonTextureCollection read_button_textures(char* path) {
+	ButtonTextureCollection textures;
+
+	Image img_base = LoadImage(path);
+
+	int width = img_base.width / 3;
+
+	Image working_img = ImageFromImage(
+		img_base,
+		(Rectangle) { 0, 0, width, img_base.height }
+	);
+	textures.normal = LoadTextureFromImage(working_img);
+	UnloadImage(working_img);
+
+	working_img = ImageFromImage(
+		img_base,
+		(Rectangle) { width, 0, width, img_base.height }
+	);
+	textures.pressed = LoadTextureFromImage(working_img);
+	UnloadImage(working_img);
+
+	working_img = ImageFromImage(
+		img_base,
+		(Rectangle) { width * 2, 0, width, img_base.height }
+	);
+	textures.hover = LoadTextureFromImage(working_img);
+	UnloadImage(working_img);
+
+	return textures;
+}
+
 char* find_image(char* storage) {
     const char *patterns[] = {
         "./cache/bgimage/%s.png",
         "./cache/bgimage/%s.jpg",
+        "./cache/image/%s.png",
+        "./cache/image/%s.jpg",
+        "./cache/fgimage/%s.png",
+        "./cache/fgimage/%s.jpg",
         NULL
     };
 
@@ -176,14 +235,24 @@ bool run_command(CommandNode* command, FataState* state) {
             assert(false);
         }
 
-        if (layer->valid) {
+        if (layer->texture_valid) {
             UnloadTexture(layer->texture);
         }
 
         Image img = LoadImage(path);
         layer->texture = LoadTextureFromImage(img);
-        layer->valid = true;
+        layer->texture_valid = true;
         UnloadImage(img);
+
+        int top_px = get_arg_int(args, "top");
+		if (top_px != NO_ARG_INT) {
+			layer->texture_offset.y = top_px;
+		}
+
+        int left_px = get_arg_int(args, "left");
+		if (left_px != NO_ARG_INT) {
+			layer->texture_offset.x = left_px;
+		}
 
         printf("Load from: %s\n", path);
     } else if (strcmp("jump", cmd) == 0) {
@@ -191,9 +260,10 @@ bool run_command(CommandNode* command, FataState* state) {
         char* path = NULL;
 
         if (storage) {
-            char path_buffer[256];
-            snprintf(path_buffer, sizeof(path_buffer), "cache/scenario/%s", storage);
+            char* path_buffer = malloc(256);
+            snprintf(path_buffer, 256, "cache/scenario/%s", storage);
             path = path_buffer;
+			printf("Path: '%s'\n", path);
         }
 
         char* target = get_arg_str(args, "target");
@@ -227,8 +297,57 @@ bool run_command(CommandNode* command, FataState* state) {
         // STOP!
         state->stopped = true;
         return true;
-    } else if (strcmp("position", cmd) == 0) {
-    }
+    } else if (strcmp("locate", cmd) == 0) {
+        int x = get_arg_int(args, "x");
+        assert(x != NO_ARG_INT);
+		state->visual.pointer_pos.x = x;
+
+        int y = get_arg_int(args, "y");
+        assert(y != NO_ARG_INT);
+		state->visual.pointer_pos.y = y;
+    } else if (strcmp("current", cmd) == 0) {
+		char* layer = get_arg_str(args, "layer");
+		assert(layer);
+
+		if (strcmp("message0", layer) == 0) {
+			state->visual.active_layer = &state->visual.fore.message_layer_zero;
+		} else if (strcmp("message1", layer) == 0) {
+			state->visual.active_layer = &state->visual.fore.message_layer_one;
+		} else {
+			assert(false);
+		}
+    } else if (strcmp("button", cmd) == 0) {
+		// button graphic:選択ライン hint:"Exit the game." target:*end enterse:button
+		char* storage = get_arg_str(args, "graphic");
+		assert(storage);
+        char* path = find_image(storage);
+        assert(path);
+
+		printf(
+			"Creating button from path: '%s' at (%d, %d)\n",
+			path,
+			state->visual.pointer_pos.x,
+			state->visual.pointer_pos.y
+		);
+
+        char* target = get_arg_str(args, "target");
+        if (target) {
+            assert(target[0] == '*');
+            target++;
+        }
+
+		Button* button = malloc(sizeof(Button));
+		button->target = target;
+		button->position = state->visual.pointer_pos;
+
+        button->textures = read_button_textures(path);
+
+		v_append(&state->visual.active_layer->children, button);
+    } else if (strcmp("close", cmd) == 0) {
+		printf("Goodbye from The House in Fata Morgana...\n");
+		exit(0);
+		return true;
+	}
 
     return false;
 }
@@ -239,7 +358,28 @@ double get_time_ms() {
     return ((tv.tv_sec) * 1000.0d) + (tv.tv_usec / 1000.0d);
 }
 
+void stop_transition(FataState* state) {
+	// ptrdiff_t offset = (char*) state->visual.active_layer - (char*) &state->visual.fore;
+	// VisualLayer* back_layer = (VisualLayer*)((char*)&state->visual.back + offset);
+	// *state->visual.active_layer = *back_layer;
+	state->visual.fore = state->visual.back;
+
+	state->transition_max_ms = 0.0f;
+	state->transition_remaining_ms = 0.0f;
+	state->wait_for_transition = false;
+	state->can_skip_transition = false;
+}
+
 void frame_work(FataState* state, double delta_ms) {
+    if (state->transition_remaining_ms > 0.0f) {
+        state->transition_remaining_ms -= delta_ms;
+        if (state->transition_remaining_ms <= 0.0f) {
+			stop_transition(state);
+        } else if (state->wait_for_transition) {
+            return;
+        }
+    }
+
     if (state->stopped) return;
 
     if (state->wait_timer_ms > 0.0f) {
@@ -247,16 +387,6 @@ void frame_work(FataState* state, double delta_ms) {
         if (state->wait_timer_ms < 0.0f) {
             state->wait_timer_ms = 0.0f;
         } else {
-            return;
-        }
-    }
-
-    if (state->transition_remaining_ms > 0.0f) {
-        state->transition_remaining_ms -= delta_ms;
-        if (state->transition_remaining_ms < 0.0f) {
-            state->transition_remaining_ms = 0.0f;
-            state->wait_for_transition = false;
-        } else if (state->wait_for_transition) {
             return;
         }
     }
@@ -272,35 +402,93 @@ void frame_work(FataState* state, double delta_ms) {
     }
 }
 
+void init_layer(VisualLayer* layer, char* name) {
+	layer->texture_valid = false;
+	layer->texture_offset = (Vec2) { 0, 0 };
+	layer->name = name;
+	layer->children = v_new();
+}
+
 void init_page(VisualPage* page) {
-	page->base_layer.valid = false;
-	page->layer_zero.valid = false;
-	page->layer_one.valid = false;
-	page->layer_two.valid = false;
-	page->message_layer_zero.valid = false;
-	page->message_layer_one.valid = false;
+	init_layer(&page->base_layer, "base");
+	init_layer(&page->layer_zero, "0");
+	init_layer(&page->layer_one, "1");
+	init_layer(&page->layer_two, "2");
+	init_layer(&page->message_layer_zero, "message0");
+	init_layer(&page->message_layer_one, "message1");
 }
 
-void draw_layer(VisualLayer* layer) {
-    if (!layer->valid) return;
+void draw_layer(FataState* state, VisualLayer* layer, Vector2 mouse_pos) {
+    if (layer->texture_valid) {
+		//printf("Drawing a texture on '%s'\n", layer->name);
+		DrawTexture(
+			layer->texture,
+			layer->texture_offset.x,
+			layer->texture_offset.y,
+			WHITE
+		);
+	}
 
-    DrawTexture(layer->texture, 0, 0, WHITE);
+	for (int i=0; i<layer->children.length;i++) {
+		Button* button = v_get(&layer->children, i);
+
+		int pos_x = button->position.x;
+		int pos_y = button->position.y;
+
+		//printf(
+		//	"Drawing button %d on '%s' at (%d, %d)\n",
+		//	i,
+		//	layer->name,
+		//	pos_x,
+		//	pos_y
+		//);
+
+		Rectangle rect = (Rectangle) {
+			pos_x,
+			pos_y,
+			button->textures.normal.width,
+			button->textures.normal.height
+		};
+
+
+
+		bool mouse_inside = CheckCollisionPointRec(mouse_pos, rect);
+
+		DrawTexture(
+			mouse_inside ? button->textures.hover : button->textures.normal,
+			pos_x,
+			pos_y,
+			WHITE
+		);
+
+		DrawRectangleLinesEx(rect, 1.0f, RED);
+
+		if (mouse_inside && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+			load(state, NULL, button->target);
+		}
+	}
 }
 
-void draw_page(VisualPage* page) {
-    draw_layer(&page->base_layer);
-    draw_layer(&page->layer_zero);
-    draw_layer(&page->layer_one);
-    draw_layer(&page->layer_two);
-    draw_layer(&page->message_layer_zero);
-    draw_layer(&page->message_layer_one);
+void draw_page(FataState* state, VisualPage* page, Vector2 mouse_pos) {
+	//printf("Drawing Page: '%s'\n", page->name);
+    draw_layer(state, &page->base_layer, mouse_pos);
+    draw_layer(state, &page->layer_zero, mouse_pos);
+    draw_layer(state, &page->layer_one, mouse_pos);
+    draw_layer(state, &page->layer_two, mouse_pos);
+    draw_layer(state, &page->message_layer_zero, mouse_pos);
+    draw_layer(state, &page->message_layer_one, mouse_pos);
 }
 
 int main() {
-	printf("FataMoru!! ^-^\n");
+	//printf("FataMoru!! ^-^\n");
 
     FataState state;
 
+	state.visual.active_layer = &state.visual.fore.message_layer_zero;
+	state.visual.pointer_pos = (Vec2) { 0, 0 };
+
+	state.visual.fore.name = "fore";
+	state.visual.back.name = "back";
 	init_page(&state.visual.fore);
 	init_page(&state.visual.back);
 
@@ -334,8 +522,7 @@ int main() {
 				state.can_skip_transition &&
 				state.transition_remaining_ms > 0.0f
 			) {
-				state.transition_remaining_ms = 0.0f;
-				state.can_skip_transition = false;
+				stop_transition(&state);
 			} else if (
 				state.can_skip_wait &&
 				state.wait_timer_ms > 0.0f
@@ -345,22 +532,26 @@ int main() {
 			}
 		}
 
+		Vector2 mouse_pos = GetMousePosition();
+
         frame_work(&state, delta_ms);
 
         BeginTextureMode(fore_target);
-            draw_page(&state.visual.fore);
+            draw_page(&state, &state.visual.fore, mouse_pos);
         EndTextureMode();
 
         // Transition if needed
         float fore_to_back_fade = 0.0;
         if (state.transition_max_ms > 0.0f) {
+			//printf("TMax: %f ... TRem: %f\n", state.transition_max_ms, state.transition_remaining_ms);
             float trans_progress_ms = state.transition_max_ms - state.transition_remaining_ms;
             fore_to_back_fade = trans_progress_ms / state.transition_max_ms;
         }
 
         if (fore_to_back_fade > 0.0f) {
+			//printf("ForeBackFade: %f\n", fore_to_back_fade);
             BeginTextureMode(back_target);
-                draw_page(&state.visual.back);
+                draw_page(&state, &state.visual.back, mouse_pos);
             EndTextureMode();
         }
 
