3062f47236910c966801c6d3a803a0db5b4650b9
Claire <onesomegit@outlook.com>
2025-12-18 00:02:52 -0600

    JACOPO

--- run-3ds.sh
+++ run-3ds.sh
@@ -23,8 +23,9 @@ echo "Compiling C..."
 echo "Linking..."
 "$CC" -specs=3dsx.specs -Wl,--wrap=printf $ARCH *.o $LIBPATHS $LIBS -o 3ds/out/output.elf
 
-ICON="$DEVKITPRO/libctru/default_icon.png"
-"$DEVKITPRO/tools/bin/smdhtool" --create "The House in Fata Morgana" "Interpreter" "Claire" $ICON 3ds/out/output.smdh
+ICON="cache/fgimage/ヤコポ_左楽.png"
+convert $ICON -trim +repage -resize "48x48^" -gravity center -extent 48x48 cache/icon.png
+"$DEVKITPRO/tools/bin/smdhtool" --create "The House in Fata Morgana" "Written by Novectacle" "Claire" cache/icon.png 3ds/out/output.smdh
 
 echo "Creating 3DSX..."
 "$TOOL3DSX" 3ds/out/output.elf 3ds/out/output.3dsx --romfs=3ds/romfs --smdh=3ds/out/output.smdh
